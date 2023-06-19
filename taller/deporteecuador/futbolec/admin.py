34c306f9a0144c526917469756331a761145328c
jandryjaramillo <jandry_josue16@hotmail.com>
2023-06-19 14:56:20 -0500

    fin1

--- taller/deporteecuador/futbolec/admin.py
+++ taller/deporteecuador/futbolec/admin.py
@@ -1,45 +1,26 @@
 from django.contrib import admin
 
 # Importar las clases del modelo
-from administrativo.models import Estudiante, Modulo, Matricula
+from futbolec.models import Jugador, Equipo, Campeonato, CampeonatoEquipos
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
 
-# Se crea la clase heredada de ModelResource
-# con el objetivo hacer exclude  para la importación
-class EstudianteResource(resources.ModelResource):
-    class Meta:
-        model = Estudiante
-        exclude = ('modulos', )
+class JugadorAdmin(ImportExportModelAdmin, admin.ModelAdmin):   
+    list_display = ('nombre', 'posicion', 'nroCamiseta', 'sueldo', 'equipo')
+    search_fields = ('nombre', 'posicion', 'equipo')
 
-# Se crea una clase que hereda
-# de ModelAdmin para el modelo
-# Estudiante
-class EstudianteAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    # se vincula con la clase EstudianteResource
-    resource_class = EstudianteResource
-    # listado de atributos que se mostrará
-    # por cada registro
-    # se deja de usar la representación (str)
-    # de la clase
-    list_display = ('id','nombre', 'apellido', 'cedula', 'edad', 'tipo_estudiante')
-    search_fields = ('nombre', 'cedula')
-    exclude = ("modulos",) # se excluye de la interfaz del admin
+admin.site.register(Jugador, JugadorAdmin)
 
-# admin.site.register se lo altera
-# el primer argumento es el modelo (Estudiante)
-# el segundo argumento la clase EstudianteAdmin
-admin.site.register(Estudiante, EstudianteAdmin)
+class EquipoAdmin(ImportExportModelAdmin, admin.ModelAdmin):   
+    list_display = ('nombre', 'siglas', 'user')
+    search_fields = ('nombre', 'siglas', 'user')
 
-admin.site.register(Modulo)
+admin.site.register(Equipo, EquipoAdmin)
 
-# admin.site.register(Matricula)
-class MatriculaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    # listado de atributos que se mostrará
-    # por cada registro
-    # se deja de usar la representación (str)
-    # de la clase
-    list_display = ('estudiante', 'modulo', 'comentario')
-    search_fields = ('estudiante__nombre', 'modulo__nombre')
+class CampeonatoAdmin(ImportExportModelAdmin, admin.ModelAdmin):   
+    list_display = ('nombre_de_campeonato', 'nombre_de_auspiciante')
+    search_fields = ('nombre_de_campeonato', 'nombre_de_auspiciante')
 
-admin.site.register(Matricula, MatriculaAdmin)
+admin.site.register(Campeonato, CampeonatoAdmin)
+
+admin.site.register(CampeonatoEquipos)
--- taller/deporteecuador/futbolec/models.py
+++ taller/deporteecuador/futbolec/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class Equipo(models.Model):
+    
+    nombre = models.CharField(max_length=30)
+    siglas = models.CharField(max_length=30)
+    user = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return "%s - %s - %s" % (self.nombre,
+                                 self.siglas,
+                                 self.user)
+
 class Jugador(models.Model):
 
     opciones_posicion_jugador = (
@@ -10,47 +21,37 @@ class Jugador(models.Model):
         )
 
     nombre = models.CharField(max_length=30)
-    posicion_campo = models.CharField(max_length=30, \
+    posicion = models.CharField(max_length=30, \
             choices=opciones_posicion_jugador)
-    nro_camiseta = models.IntegerField("Número de camiseta", unique=True)
+    nroCamiseta = models.IntegerField("Número de camiseta", unique=True)
     sueldo = models.IntegerField("Sueldo")  
 
-    equipo_de_futbol = models.ManyToManyField('Modulo', through='Matricula')
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
 
 
     def __str__(self):
-        return "%s - %s - %s - edad: %d - tipo: %s" % (self.nombre,
-                self.apellido,
-                self.cedula,
-                self.edad,
-                self.tipo_estudiante)
+        return "%s - %s - %d - %d" % (self.nombre,
+                self.posicion,
+                self.nroCamiseta,
+                self.sueldo)
 
-
-class Modulo(models.Model):
-    """
-    """
-    opciones_modulo = (
-        ('1', 'Primero'),
-        ('2', 'Segundo'),
-        )
-
-    nombre = models.CharField(max_length=30, \
-            choices=opciones_modulo)
-    estudiantes = models.ManyToManyField(Estudiante, through='Matricula')
-
+class Campeonato(models.Model):
+    
+    nombre_de_campeonato = models.CharField(max_length=30)
+    nombre_de_auspiciante = models.CharField(max_length=30)    
+  
     def __str__(self):
-        return "Módulo: %s" % (self.nombre)
+        return "%s - %s" % (self.nombre_de_campeonato,
+                            self.nombre_de_auspiciante)
 
 
 class CampeonatoEquipos(models.Model):
-    """
-    """
-    estudiante = models.ForeignKey(Estudiante, related_name='lasmatriculas',
-            on_delete=models.CASCADE)
-    modulo = models.ForeignKey(Modulo, related_name='lasmatriculas',
-            on_delete=models.CASCADE)
-    comentario = models.CharField(max_length=200)
-
+    
+    año = models.IntegerField("Año")
+    
+    equipo_de_futbol = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    campeonato = models.ForeignKey(Campeonato, on_delete=models.CASCADE)
+    
     def __str__(self):
-        return "Matricula: Estudiante(%s) - Modulo(%s)" % \
-                (self.estudiante, self.modulo.nombre)
+        return "Campeonato Equipos: Año: %d - Equipo(%s) - Campeonato(%s)" % \
+                (self.año, self.equipo_de_futbol, self.campeonato)
